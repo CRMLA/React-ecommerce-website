0aefb8bd52ecb1eb58ff75e41f871b36b8d2a9ea
CRMLA <abdo.abdo5830@gmail.com>
2024-05-17 16:19:51 +0200

    Add New Features

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavItems from "./components/NavItems";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <NavItems />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from './Banner'
+import HomeCatagory from './HomeCatagory'
+import CategoryShowCase from './CategoryShowCase'
+
+const Home = () => {
+  return (
+    <div>
+     <Banner />
+     <HomeCatagory />
+     <CategoryShowCase />
+    </div>
+  )
+}
+
+export default Home
